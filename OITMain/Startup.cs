479b296bce39f0edc6c1985d189babeddd6dd4e3
abalde <abalde@DITL-ARL-SYSA>
2017-02-28 16:26:23 -0500

    Add project files.

---
+++ OITMain/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OITMain.Startup))]
+namespace OITMain
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
