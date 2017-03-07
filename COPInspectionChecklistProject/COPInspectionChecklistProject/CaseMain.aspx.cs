76ab7b43a36769b4f3dc1aef30e4b55d07e0720a
draconhawke <draconhawke@yahoo.com>
2017-03-07 11:34:56 -0500

    updated CaseMain

--- COPInspectionChecklistProject/COPInspectionChecklistProject/CaseMain.aspx.cs
+++ COPInspectionChecklistProject/COPInspectionChecklistProject/CaseMain.aspx.cs
@@ -8,40 +8,53 @@ namespace COPInspectionChecklistProject
 {
     public partial class CaseMain : Page
     {
-        public string caseNumber;
+        Case newCase = new Case();
         string conn = ConfigurationManager.ConnectionStrings["OITDB"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
+            //if page loads as current case, run this code
             if (Request.QueryString["CaseNumber"] != null)
             {
-                caseNumber = Request.QueryString["CaseNumber"];
-                Case caseNo = getCaseByCaseNumber(caseNumber);
-                insertData(caseNo);
+                string caseNumber = Request.QueryString["CaseNumber"];
+                getCaseByCaseNumber(caseNumber);
+                insertData();
             }
+            //else data is loaded as a blank page, with a generated caseNumber
+            txtCaseNum.Text = newCase.caseNumber;
         }
-        protected void btnSave_Click(object sender, EventArgs e)
-        {
-            SqlDataAdapter ada = new SqlDataAdapter("Insert into CaseMain('" + txtCaseNum + "'.'" + txtPropAdd + "','" + txtRespParty
-                    + "','" + txtMailAdd + "','" + txtEmail + "','" + txtAppPhone + "','" + txtOccDwell + "','" + txtNumUnits + "','" + txtOwnerName
-                    + "','" + txtOwnerPhone + "','" + txtSidewalk + "','" + txtInspector + "','" + txtInspectEmail + "','" + txtInspectDate + "','"
-                    + txtReinspectDate + ")", conn);
-            DataSet ds = new DataSet();
-            ada.Fill(ds);
+        /// <summary>
+        /// Save button updates database for new Case with current attributes
+        /// currently holds dummy info to save data to database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSave_Click(object sender, EventArgs e) {
+            //SqlDataAdapter ada = new SqlDataAdapter("Insert into CaseMain('" + txtCaseNum + "'.'" + txtPropAdd + "','" + txtRespParty
+            //        + "','" + txtMailAdd + "','" + txtEmail + "','" + txtAppPhone + "','" + txtOccDwell + "','" + txtNumUnits + "','" + txtOwnerName
+            //        + "','" + txtOwnerPhone + "','" + txtSidewalk + "','" + txtInspector + "','" + txtInspectEmail + "','" + txtInspectDate + "','"
+            //        + txtReinspectDate + ")", conn);
+            //DataSet ds = new DataSet();
+            //ada.Fill(ds);
         }
-        protected void btnDelete_Click(object sender, EventArgs e)
-        {
-            using (SqlConnection con = new SqlConnection(conn))
-            {
-                SqlCommand cmd = new SqlCommand("Delete from tblCaseMain where caseNumber = @CaseNumber", con);
-                cmd.CommandType = CommandType.StoredProcedure;
+        /// <summary>
+        /// Delete button removes selected CaseNumber from database
+        /// currently holds dummy data to remove data from database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnDelete_Click(object sender, EventArgs e) {
+        //    using (SqlConnection con = new SqlConnection(conn))
+        //    {
+        //        SqlCommand cmd = new SqlCommand("Delete from tblCaseMain where caseNumber = @CaseNumber", con);
+        //        cmd.CommandType = CommandType.StoredProcedure;
 
-                SqlParameter parameter = new SqlParameter("@CaseNumber", caseNumber);
-                cmd.Parameters.Add(parameter);
-                con.Open();
-                cmd.ExecuteNonQuery();
-            }
+        //        SqlParameter parameter = new SqlParameter("@CaseNumber", caseNumber);
+        //        cmd.Parameters.Add(parameter);
+        //        con.Open();
+        //        cmd.ExecuteNonQuery();
+        //    }
         }
-        public Case getCaseByCaseNumber(string caseNumber)
+        public void getCaseByCaseNumber(string caseNumber)
         {
             Case caseNo = new Case();
             using (SqlConnection con = new SqlConnection(conn))
@@ -54,9 +67,9 @@ namespace COPInspectionChecklistProject
                 con.Open();
                 SqlDataReader rdr = cmd.ExecuteReader();
             }
-            return caseNo;
+            newCase = caseNo;
         }
-        public void insertData(Case newCase)
+        public void insertData()
         {
             txtCaseNum.Text = newCase.caseNumber;
             txtPropAdd.Text = newCase.propertyAddress;
@@ -73,10 +86,6 @@ namespace COPInspectionChecklistProject
             txtInspectEmail.Text = newCase.inspectorEmail;
             txtInspectDate.Text = newCase.inspectionDate.ToString();
             txtReinspectDate.Text = newCase.reinspectionDate.ToString();
-        }
-        public void deleteData(string name)
-        {
-
         }
         protected void btnInspectionChecklist_Click(object sender, EventArgs e)
         {
