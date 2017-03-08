ce8bf06294e97a8c341aa7b8a21c695c4dd18c75
Jason <Jason@JASON-HP>
2017-03-07 19:13:54 -0500

    Redirect to mainmenu.aspx

--- COPInspectionChecklistProject/COPInspectionChecklistProject/Default.aspx.cs
+++ COPInspectionChecklistProject/COPInspectionChecklistProject/Default.aspx.cs
@@ -10,8 +10,8 @@ namespace COPInspectionChecklistProject
     public partial class _Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
+        {            
+            Response.Redirect("MainMenu.aspx");
         }
     }
 }
