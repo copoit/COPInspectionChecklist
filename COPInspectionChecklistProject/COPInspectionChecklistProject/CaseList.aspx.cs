41c61db46a227e4b1b12668b916aa3fbbea3e0c7
chaodehe <dehanhe@gmail.com>
2017-03-17 19:08:38 -0400

    Remove unnecessary codes

--- COPInspectionChecklistProject/COPInspectionChecklistProject/CaseList.aspx.cs
+++ COPInspectionChecklistProject/COPInspectionChecklistProject/CaseList.aspx.cs
@@ -3,7 +3,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Collections;
 using COPInspectionChecklistProject.Common;
-using System.Data;
+
 
 namespace COPInspectionChecklistProject
 {
