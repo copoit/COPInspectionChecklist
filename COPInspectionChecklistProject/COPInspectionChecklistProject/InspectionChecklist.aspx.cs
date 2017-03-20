9d89924f76892344828f092af7f5ab41b3c5c397
bibsson <bibsson@hotmail.com>
2017-03-19 20:24:03 -0400

    Update InspectionChecklist.aspx.cs

--- COPInspectionChecklistProject/COPInspectionChecklistProject/InspectionChecklist.aspx.cs
+++ COPInspectionChecklistProject/COPInspectionChecklistProject/InspectionChecklist.aspx.cs
@@ -142,22 +142,7 @@ namespace COPInspectionChecklistProject
          
           protected void btnEmail_Click(object sender, EventArgs e)
           //Sending email
-         {
-          try
-         {
-        MailMessage mailMessage = new MailMessage();
-        mailMessage.To.Add("your.own@mail-address.com");
-        mailMessage.From = new MailAddress("another@mail-address.com");
-        mailMessage.Subject = "Inpection Deficiencies";
-        mailMessage.Body = "Inspection Deficiencies report";
-        SmtpClient smtpClient = new SmtpClient("smtp.your-isp.com");
-        smtpClient.Send(mailMessage);
-        Response.Write("E-mail sent!");
-        }
-         catch(Exception ex)
         {
-        Response.Write("Could not send the e-mail - error: " + ex.Message);
-        }
 
         protected void btnCertificateInspection_Click(object sender, EventArgs e)
         {
