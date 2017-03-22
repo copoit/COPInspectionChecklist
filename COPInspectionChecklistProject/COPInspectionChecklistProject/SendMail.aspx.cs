580255b356b4b229de8a0a82b44bc8d2efc9d7b3
bibsson <bibsson@hotmail.com>
2017-03-22 16:35:27 -0400

    Update SendMail.aspx.cs

--- COPInspectionChecklistProject/COPInspectionChecklistProject/SendMail.aspx.cs
+++ COPInspectionChecklistProject/COPInspectionChecklistProject/SendMail.aspx.cs
@@ -13,12 +13,12 @@ using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
 
 
-namespace MailWebService
+namespace COPInspectionChecklistProject
 {
     /// <summary>
-    /// Summary description for WebForm1.
+    /// Summary description for SendMail.
     /// </summary>
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class SendForm : System.Web.UI.Page
     {
         protected System.Web.UI.WebControls.RadioButtonList rblMailFormat;
         protected System.Web.UI.WebControls.Button btnSend;
