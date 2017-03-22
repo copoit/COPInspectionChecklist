0ecdeb61340a5a2a650c1f88717abed48bce69c5
bibsson <bibsson@hotmail.com>
2017-03-22 10:37:39 -0400

    Update SendMail.aspx.cs

--- COPInspectionChecklistProject/COPInspectionChecklistProject/SendMail.aspx.cs
+++ COPInspectionChecklistProject/COPInspectionChecklistProject/SendMail.aspx.cs
@@ -48,13 +48,7 @@ namespace COPInspectionChecklistProject
 		/// Required method for Designer support - do not modify
 		/// the contents of this method with the code editor.
 		/// </summary>
-		private void InitializeComponent()
-		{    
-			this.btnSendMail.Click += new System.EventHandler(this.btnSendMail_Click);
-			this.Load += new System.EventHandler(this.Page_Load);
-
-		}
-		#endregion
+		
 
 		private void btnSendMail_Click(object sender, System.EventArgs e)
 		{
