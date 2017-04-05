91eb7e2a2495392550a7177c53d59eaa1613e9aa
bibsson <bibsson@hotmail.com>
2017-04-05 15:49:22 -0400

    Update InspectionChecklist.aspx.cs

--- COPInspectionChecklist/COPInspectionChecklistProject/InspectionChecklist.aspx.cs
+++ COPInspectionChecklist/COPInspectionChecklistProject/InspectionChecklist.aspx.cs
@@ -229,7 +229,7 @@ private void EmailInspection(string caseNumber)
                             + "VIOLATIONS ON CASE_INFO.Case_Num = VIOLATIONS.Case_Num INNER JOIN CL_SectionDetail"
                             +  "ON CL_SectionDetail.SubSection_ID = VIOLATIONS.SubSection_ID"
                             + "WHERE CASE_INFO.Case_Num = '" + caseNumber + "'";
-                            + "ORDER BY CL_SectionDetail.SubSectionSeq_ID;
+                         
                             
             var dt = clsCommon.TestDBConnection(SQL);
 
