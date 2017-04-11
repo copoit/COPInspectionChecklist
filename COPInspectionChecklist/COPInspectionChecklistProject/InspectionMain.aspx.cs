6993dde6fc4778108b038a9cb1b5c13668c8e7f7
bibsson <bibsson@hotmail.com>
2017-04-11 11:30:20 -0400

    Update InspectionMain.aspx.cs

--- COPInspectionChecklist/COPInspectionChecklistProject/InspectionMain.aspx.cs
+++ COPInspectionChecklist/COPInspectionChecklistProject/InspectionMain.aspx.cs
@@ -1,4 +1,4 @@
-﻿using COPInspectionChecklistProject.Common;
+using COPInspectionChecklistProject.Common;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -36,15 +36,16 @@ namespace COPInspectionChecklistProject
 
         protected void btnProperty_Click(object sender, EventArgs e)
         {
+            //if (txtStreetNum.Text.Trim() != "" && txtStreetName.Text.Trim() != "" && txtZip.Text.Trim() != "")
+            //{
+            //    SearchForProperty(txtStreetNum.Text, txtStreetName.Text, txtZip.Text);
+            //}
+            //else
+            //{
+            //    lblError.Text = "Please enter a property.";
+            //}
 
-            if (txtStreetNum.Text.Trim() != "" && txtStreetName.Text.Trim() != "" && txtZip.Text.Trim() != "")
-            {
-                SearchForProperty(txtStreetNum.Text, txtStreetName.Text, txtZip.Text);
-            }
-            else
-            {
-                lblError.Text = "Please enter a property.";
-            }
+            SearchForProperty(txtStreetNum.Text, txtStreetName.Text, txtZip.Text);
         }
 
         private void SearchForCase(string Case)
@@ -68,16 +69,30 @@ namespace COPInspectionChecklistProject
         private void SearchForProperty(string StreetNum, string StreetName, string Zip)
         {
             DbCommon clsCommon = new DbCommon();
-            string SQL = "SELECT Property_ID FROM [PROPERTY_INFO] where Property_StreetNumber = '" + StreetNum + "' AND Property_StreetName = '" + StreetName.ToUpper() + "' AND Property_Zip = '" + Zip + "'";
+
+            string SQL = "SELECT * FROM [PROPERTY_INFO] WHERE 1 = 1 "
+                            + (StreetNum != "" ? string.Format("AND Property_StreetNumber LIKE '%{0}%' ", StreetNum) : "")
+                            + (StreetName != "" ? string.Format("AND Property_StreetName LIKE '%{0}%' ", StreetName) : "")
+                            + (Zip != "" ? string.Format("AND Property_Zip LIKE '%{0}%' ", Zip) : "");
+
+            //string SQL = "SELECT Property_ID FROM [PROPERTY_INFO] where Property_StreetNumber = '" + StreetNum + "' AND Property_StreetName = '" + StreetName.ToUpper() + "' AND Property_Zip = '" + Zip + "'";
             var dt2 = clsCommon.TestDBConnection(SQL);
 
-            if (dt2.Rows.Count > 0)
+            if (dt2.Rows.Count == 1)
             {
                 Session["PropertyID"] = dt2.Rows[0]["Property_ID"];
                 Response.Redirect("PropertyMain.aspx?PropertyID=" + dt2.Rows[0]["Property_ID"]);
             }
+            else if (dt2.Rows.Count > 1)
+            {
+                gvProperties.DataSource = dt2;
+                gvProperties.DataBind();
+            }
             else
             {
+                gvProperties.DataSource = null;
+                gvProperties.DataBind();
+
                 lblError.Text = "No Property found.  Please enter a Street Number, Street Name, and Zip.";
             }
 
