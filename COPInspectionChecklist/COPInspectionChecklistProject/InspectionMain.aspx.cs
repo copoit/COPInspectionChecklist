7c307299c64b9946d228803d5c50a3c732d46453
bibsson <bibsson@hotmail.com>
2017-04-12 09:48:10 -0400

    Update InspectionMain.aspx.cs

--- COPInspectionChecklist/COPInspectionChecklistProject/InspectionMain.aspx.cs
+++ COPInspectionChecklist/COPInspectionChecklistProject/InspectionMain.aspx.cs
@@ -9,7 +9,7 @@ using System.Web.UI.WebControls;
 
 namespace COPInspectionChecklistProject
 {
-    public partial class InspectionMain : System.Web.UI.Page
+    public partial class WebForm2 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
