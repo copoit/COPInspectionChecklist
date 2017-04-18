b6aca65c7dc0a0e49796b8163678392ede72d01b
chaodehe <dehanhe@gmail.com>
2017-04-18 18:22:47 -0400

    add calendar library references

---
+++ COPInspectionChecklist/COPInspectionChecklistProject/InspectorReport.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="InspectorReport.aspx.cs" Inherits="COPInspectionChecklistProject.InspectorReport" %>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server"> 
+    <div> 
+	    <div>  
+	        <b>Inspector Report</b>           
+            <br />
+            <br />
+            <asp:Table ID="InspectorReportTable" runat="server">
+	            <asp:TableRow>
+                    <asp:TableCell>Inspector Name:</asp:TableCell>
+                    <asp:TableCell>
+                         <asp:DropDownList ID="ddlInspector" runat="server" Width="300px" AutoPostBack="true" DataTextField = "InspectorName" DataValueField = "Inspector_ID" OnSelectedIndexChanged="ddlInspector_SelectedIndexChanged" ></asp:DropDownList>
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+ 	        <br />
+            <br /> 
+            <asp:GridView ID="CaseListGridView" runat="server" autogeneratecolumns="false">
+                <Columns>
+                    <asp:HyperLinkField HeaderText="Case Number" DataTextField="Case_Num" DataNavigateUrlFields="Case_Num" DataNavigateUrlFormatString="CaseMain.aspx?CaseNumber={0}" /> 
+                    <asp:BoundField HeaderText="Inspector"  DataField="Inspector" />
+                    <asp:BoundField HeaderText="Inspection Date"  DataField="Inspection_Date" DataFormatString="{0:d}" />
+                    <asp:BoundField HeaderText="Inspection Type" DataField="Inspection_Type" />
+                    <asp:BoundField HeaderText="Inspection Status" DataField="Inspection_Status"  />
+                </Columns>
+            </asp:GridView>
+            <br />
+ 	    </div>	   
+    </div>
+</asp:Content>
---
+++ COPInspectionChecklist/COPInspectionChecklistProject/InspectorReport.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using COPInspectionChecklistProject.Common;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace COPInspectionChecklistProject
+{
+    public partial class InspectorReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!IsPostBack)
+                {
+                    getInspectorList();
+                }
+            }catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+        }
+
+        private void retrieveCaseByInspectorId(String InspectorID)
+        {
+            DbCommon clsCommon = new DbCommon();
+            
+            DataTable dataTable = new DataTable();
+            DataSet caseListDataSet = new DataSet();
+            try
+            {
+
+                //retrieve all cases by property Id
+                string SQL = "SELECT CASE_INFO.Case_Num,CASE_INFO.Inspection_Date, CASE_INFO.ReInspection_Date,CASE_INFO.Inspection_Status," +
+                    " CASE_INFO.Inspection_Type,INSPECTOR_INFO.Inspector_FName+' '+INSPECTOR_INFO.Inspector_LName as Inspector " +
+                    "  FROM CASE_INFO left join INSPECTOR_INFO ON CASE_INFO.Inspector_ID=INSPECTOR_INFO.Inspector_ID  Where CASE_INFO.Inspector_ID ='" + InspectorID + "'";
+
+                var caseListDT = clsCommon.TestDBConnection(SQL);
+                if (caseListDT.Rows.Count > 0)
+                {
+                    dataTable = caseListDT;
+                }
+
+                caseListDataSet.Tables.Add(dataTable);
+                CaseListGridView.DataSource = caseListDataSet.Tables[0];
+                CaseListGridView.DataBind();
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            // return caseList;
+        }
+
+        private void getInspectorList()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBOIT"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select Inspector_ID, Inspector_FName +' '+Inspector_LName as InspectorName from Inspector_Info", conn);
+                conn.Open();
+                ddlInspector.DataSource = cmd.ExecuteReader();
+                ddlInspector.DataBind();
+                conn.Close();
+            }
+        }
+
+        protected void ddlInspector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string inspectorId = ddlInspector.SelectedValue;
+            retrieveCaseByInspectorId(inspectorId);
+        }
+    }
+
+    
+}
